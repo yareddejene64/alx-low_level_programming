d02bbb1ebc3ff83124ee826eda5914e9f1db70e1
yareddejene64$ <yareddejene64@gmail.com>
2022-09-12 03:23:49 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-*	main - print string in the put function
+*   main - print string in the put function
 *
-*	Description: using main function
-*	this program prints "Programming is like building a multilingual puzzle
-*	Return:0
+*   Description: using main function
+*   this program prints "Programming is like building a multilingual puzzle
+*   Return:0
 */
 
 int main(void)
 
 {	
-       	puts("\"Programming is like building a multilingual puzzle\n");
-	        return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
