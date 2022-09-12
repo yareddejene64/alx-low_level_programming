3ac8eacdceda7a548f72780ef2e0e4ec5c7fab09
yared dejene <111049168+yareddejene64@users.noreply.github.com>
2022-09-12 12:44:08 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-
 #include <stdio.h>
-
+/**
+*main print string in the put function
+*this program prints "Programming is like building a multilingual puzzle
+*/
 int main(void)
 {
-	    puts("Programming is like building a multilingual puzzle\n");
+	    puts("\"Programming is like building a multilingual puzzle");
 	        return (0);
 }
