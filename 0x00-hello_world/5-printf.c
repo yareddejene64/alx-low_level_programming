216218e27c89eecb638849c5024583e1d7492e6e
yareddejene64$ <yareddejene64@gmail.com>
2022-09-12 03:41:34 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the printf function
+*
+* Description: using the main function
+* this program prints "programing is like building a multilingual puzzel
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
