a4731b52cb63cfd6b93556e6dd5f9c762609bd55
yared dejene <111049168+yareddejene64@users.noreply.github.com>
2022-09-12 14:54:13 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,15 @@
 */
 int main(void)
 {
-	
 	char chartype;
 	int inttype;
 	long li;
 	long long lli;
 	float floattype;
 
-	printf("size of char : %zu byte\n",sizeof (chartype));
-	printf("size of int : %zu byte\n",sizeof (inttype));
-	printf("size of long : %zu byte\n",sizeof (li));
-	printf("size of long long : %zu byte\n",sizeof (lli));
-	printf("size of float : %zu byte\n",sizeof (floattype));
-	return(0); }
+	printf("size of char : %zu byte(s)\n",sizeof (chartype));
+	printf("size of int : %zu byte(s)\n",sizeof (inttype));
+	printf("size of long : %zu byte(s)\n",sizeof (li));
+	printf("size of long long : %zu byte(s)\n",sizeof (lli));
+	printf("size of float : %zu byte(s)\n",sizeof (floattype));
+	return (0); }
