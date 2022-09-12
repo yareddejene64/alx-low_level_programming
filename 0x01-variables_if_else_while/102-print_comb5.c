7dee257f821e66dd617ba143eb047a4158ec4a95
yareddejene64$ <yareddejene64@gmail.com>
2022-09-12 08:46:55 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations of 2 digits
+ * Return: 0
+ */
+int main(void)
+{
+    int n1 = 0, n2;
+    while(n1 <= 99)
+    {
+        n2 = n1;
+        while(n2 <= 99)
+
+{
+    if (n2 != n1)
+    {
+        putchar((n1 / 10 ) + 48 );
+        putchar((n1 % 10) + 48 );
+        putchar(' ');
+        putchar((n2 / 10) + 48);
+        putchar((n2 % 10) +  48);
+
+        if(n1 != 98 || n2 != 98)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        ++n2;
+    }
+    ++n1;
+  }
+  putchar('\n');
+  return 0;
+    }
+
+}
+
