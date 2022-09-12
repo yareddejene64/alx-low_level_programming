6bcaa713e29fb86ec023c30f9e4ff78e2c5e5591
yareddejene64$ <yareddejene64@gmail.com>
2022-09-12 06:03:12 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the string in the printf function
+ *
+ * Description: using the main function
+ * this program prints
+ * Return: 0
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+		return (0); }
