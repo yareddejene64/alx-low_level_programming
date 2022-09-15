6631099b4b2587be626daa68f3a4c6b3178f897d
yared dejene <111049168+yareddejene64@users.noreply.github.com>
2022-09-15 18:13:27 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,17 @@
  * *
  * *Return: returns 0
  */
-
 int main(void)
-
 {
-
-	int i;
-
+   int i;
 	for (i = 1; i <= 100; i++)
 {
 	if (i % 15 == 0)
-
 		printf("FizzBuzz\t");
-
 	else if ((i % 3) == 0)
-
 		printf("Fizz\t");
-
 	else if ((i % 5) == 0)
-
 		printf("Buzz\t");
-
 	else
 		printf("%d\t", i);
 }
