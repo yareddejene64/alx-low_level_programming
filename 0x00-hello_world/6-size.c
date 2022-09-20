b999cf8f332f3519f652e4935dffedfd9de0ec70
yared dejene <111049168+yareddejene64@users.noreply.github.com>
2022-09-20 18:12:54 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+
 /**
-* main - print the string in the printf function
-*
-* Description: using the main function
-* this program prints "programing is like building a multilingual puzzel
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char chartype;
-	int inttype;
-	long li;
-	long long lli;
-	float floattype;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of char: %lu byte(S)\n", (unsigned long)sizeof(chartype));
-	printf("Size of int: %lu byte(S)\n", (unsigned long)sizeof(inttype));
-	printf("Size of long int: %lu byte(S)\n", (unsigned long)sizeof(li));
-	printf("Size of long long int: %lu byte(S)\n", (unsigned long)sizeof(lli));
-	printf("Size of float: %lu byte(S)\n", (unsigned long)sizeof(floattype));
-	return (0); }
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
