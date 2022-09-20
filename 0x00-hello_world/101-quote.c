84b87f05d22cef9f80c9f78b39f4a1fb42966b08
yared dejene <111049168+yareddejene64@users.noreply.github.com>
2022-09-20 18:41:15 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - print the string in the printf function
- *
- * Description: using the main function
- * this program prints
- * Return: 0
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 	      \" - Dora Korpar, 2015-10-19\n",59);
-		return (1); 
+	return (1); 
 }
