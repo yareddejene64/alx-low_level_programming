a87462d92d7037a858a9980b268a0e86b3a5b341
yared dejene <111049168+yareddejene64@users.noreply.github.com>
2022-09-20 18:10:53 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	long long lli;
 	float floattype;
 
-	printf("Size of char: %lu byte(S)\n", sizeof(chartype));
-	printf("Size of int: %lu byte(S)\n", sizeof(inttype));
-	printf("Size of long int: %lu byte(S)\n", sizeof(li));
-	printf("Size of long long int: %lu byte(S)\n", sizeof(lli));
-	printf("Size of float: %lu byte(S)\n", sizeof(floattype));
+	printf("Size of char: %lu byte(S)\n", (unsigned long)sizeof(chartype));
+	printf("Size of int: %lu byte(S)\n", (unsigned long)sizeof(inttype));
+	printf("Size of long int: %lu byte(S)\n", (unsigned long)sizeof(li));
+	printf("Size of long long int: %lu byte(S)\n", (unsigned long)sizeof(lli));
+	printf("Size of float: %lu byte(S)\n", (unsigned long)sizeof(floattype));
 	return (0); }
