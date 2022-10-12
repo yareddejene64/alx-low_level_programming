989b71e60efca9853883ac0a8640d345e3da67d4
yareddejene64 <yareddejene64@gmail.com>
2022-10-12 18:32:30 +0300

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name.
- * @name: pointer to name to print
- * @f: function to print name
- * Return: Nothing
+ * main - prints opcode of own main function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 1 or 2 on fail, 0 on success
  */
-void print_name(char *name, void (*f)(char *name))
+int main(int argc, char *argv[])
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	int bytes, i;
+	unsigned char *func_ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	func_ptr = (unsigned char *)main;
+	i = 0;
+	if (bytes > 0)
+	{
+		while (i < (bytes - 1))
+			printf("%02hhx ", func_ptr[i++]);
+		printf("%hhx\n", func_ptr[i]);
+	}
+	return (0);
 }
 
