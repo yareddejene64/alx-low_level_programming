e5f67acbd38caf2c4e229eda929b93e4c1f208e4
yareddejene64 <yareddejene64@gmail.com>
2022-10-25 18:04:02 +0300

    main

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,4 +26,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-#endif[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[Calx[C[C[C[C[C[C[C[C[C[C[C[C[p[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C alx[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D
+#endif
